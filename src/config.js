cf59afceb4446f87b2e615b36d924028855cc95c
Briceson Roy <royzone3@ymail.com>
2020-08-11 21:58:11 -0400

    connected client origin

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  CLIENT_ORIGIN:'https://jeopardy-generator.vercel.app/',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/jeopardy-generator-capstone-server'
 }
