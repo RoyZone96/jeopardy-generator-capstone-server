31dee488edece95ab9c39afd1909fd41bdeeef06
Briceson Roy <royzone3@ymail.com>
2020-08-15 15:13:51 -0400

    fixing auth router

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,18 +1,33 @@
 const express = require('express')
+const AuthService = require('./auth-service')
 const jsonBodyParser = express.json()
 
 const authRouter = express.Router()
 
 authRouter
     .post('/login', jsonBodyParser, (req, res, next) => {
-        const { user_name, password } = req.body
-        const loginUser = { user_name, password }
+        const { username, password } = req.body
+        const loginUser = { username, password }
 
         for (const [key, value] of Object.entries(loginUser))
             if (value == null)
                 return res.status(400).json({
                     error: `Missing '${key}' in request body`
                 })
+
+        AuthService.getUserWithUserName(
+            req.app.get('db'),
+            loginUser.username
+        )
+            .then(dbUser => {
+                if (!dbUser)
+                    return res.status(400).json({
+                        error: 'Incorrect user_name or password',
+                    })
+                res.send('ok')
+            })
+            .catch(next)
+
         return AuthService.comparePasswords(loginUser.password, dbUser.password)
             .then(compareMatch => {
                 if (!compareMatch)
@@ -20,9 +35,15 @@ authRouter
                         error: 'Incorrect user_name or password',
                     })
 
-                res.send('ok')
+                const sub = dbUser.user_name
+                const payload = { user_id: dbUser.id }
+                res.send({
+                    authToken: AuthService.createJwt(sub, payload),
+                })
+                    .catch(error => { return 'this is an error message' })
             })
     })
-    .catch(next)
-                
+
+// .catch(error => {return 'this is an error message'})
+
 module.exports = authRouter
