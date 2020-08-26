aef41cb05379adfc1b9b8e4f488c6094f12c3ecb
Briceson Roy <royzone3@ymail.com>
2020-08-26 17:32:22 -0400

    changes to the board router

--- src/boards/boards-router.js
+++ src/boards/boards-router.js
@@ -16,12 +16,84 @@ const serializeBoards = boards => ({
 })
 
 boardsRouter
-  .route('/:boards_id')
+  .route('/')
   .get((req, res, next) => {
-    const knexInstance = req.app.get('db')
-    BoardsService.getBoards(knexInstance)
+    BoardsService.getBoards(
+      req.app.get('db')
+    )
       .then(boards => {
-        res.json(boards.map(serializeBoards))
+        res.json(boards)
+      })
+      .catch(next)
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { user_id,
+      board_title,
+      times_played
+    } = req.body
+    console.log(req.body)
+    const newBoards = {
+      user_id,
+      board_title,
+      times_played
+    }
+
+    for (const [key, value] of Object.entries(newBoards))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        })
+
+
+    BoardsService.insertBoards(
+      req.app.get('db'),
+      newBoards
+    )
+      .then(boards => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${boards.id}`))
+          .json(serializeBoards(boards))
+      })
+      .catch(next)
+  })
+  .all((req, res, next) => {
+    if (isNaN(parseInt(req.params.boards_id))) {
+      return res.status(404).json({
+        error: { message: `Invalid id` }
+      })
+    }
+    BoardsService.getBoards(
+      req.app.get('db'),
+      req.params.boards_id
+    )
+      .then(boards => {
+        if (!boards) {
+          return res.status(404).json({
+            error: { message: `boards doesn't exist` }
+          })
+        }
+        res.boards = boards
+        next()
+      })
+      .catch(next)
+  })
+
+boardsRouter
+  .route('/:boards_id')
+  .all((req, res, next) => {
+    BoardsService.getBoardsById(
+      req.app.get('db'),
+      req.params.boards_id
+    )
+      .then(board => {
+        if (!board) {
+          return res.status(404).json({
+            error: { message: `board doesn't exist` }
+          })
+        }
+        res.board = board
+        next()
       })
       .catch(next)
   })
@@ -35,7 +107,7 @@ boardsRouter
         console.log(numRowsAffected)
         res.status(204).end()
       })
-      .catch(err => console.log(err))
+      .catch(next)
   })
   .patch(jsonParser, (req, res, next) => {
     const { user_id,
@@ -75,65 +147,6 @@ boardsRouter
       })
       .catch(err => console.log(err))
   })
-
-boardsRouter
-  .route('/')
-  .all((req, res, next) => {
-    if (isNaN(parseInt(req.params.boards_id))) {
-      return res.status(404).json({
-        error: { message: `Invalid id` }
-      })
-    }
-    BoardsService.getBoards(
-      req.app.get('db'),
-      req.params.boards_id
-    )
-      .then(boards => {
-        if (!boards) {
-          return res.status(404).json({
-            error: { message: `boards doesn't exist` }
-          })
-        }
-        res.boards = boards
-        next()
-      })
-      .catch(next)
-  })
-  .get((req, res, next) => {
-    res.json(serializeBoards(res.boards))
-  })
-  .post(jsonParser, (req, res, next) => {
-    const { user_id,
-      board_title,
-      times_played
-    } = req.body
-    console.log(req.body)
-    const newBoards = {
-      user_id,
-      board_title,
-      times_played
-    }
-
-    for (const [key, value] of Object.entries(newBoards))
-      if (value == null)
-        return res.status(400).json({
-          error: { message: `Missing '${key}' in request body` }
-        })
-
-
-    BoardsService.insertBoards(
-      req.app.get('db'),
-      newBoards
-    )
-      .then(boards => {
-        res
-          .status(201)
-          .location(path.posix.join(req.originalUrl, `/${boards.id}`))
-          .json(serializeBoards(boards))
-      })
-      .catch(next)
-  })
-  
 
 
 
