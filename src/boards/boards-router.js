e2fece3be85ac256ad7dfbf07e8713dd1ccc7e85
Briceson Roy <royzone3@ymail.com>
2020-08-25 21:32:15 -0400

    working on boards router

--- src/boards/boards-router.js
+++ src/boards/boards-router.js
@@ -28,92 +28,6 @@ boardsRouter
 
 boardsRouter
   .route('/')
-  .post(jsonParser, (req, res, next) => {
-    const { user_id,
-      board_title,
-      times_played,
-      date_created,
-      date_updated } = req.body
-    const newBoards = {
-      user_id,
-      board_title,
-      times_played,
-      date_created,
-      date_updated
-    }
-
-    for (const [key, value] of Object.entries(newBoards))
-      if (value == null)
-        return res.status(400).json({
-          error: { message: `Missing '${key}' in request body` }
-        })
-
-
-    BoardsService.insertBoards(
-      req.app.get('db'),
-      newBoards
-    )
-      .then(boards => {
-        res
-          .status(201)
-          .location(path.posix.join(req.originalUrl, `/${boards.id}`))
-          .json(serializeBoards(boards))
-      })
-      .catch(next)
-  })
-  .delete((req, res, next) => {
-    console.log(req.params)
-    BoardsService.deleteBoards(
-      req.app.get('db'),
-      req.params.board_id
-    )
-      .then(numRowsAffected => {
-        console.log(numRowsAffected)
-        res.status(204).end()
-      })
-      .catch(err => console.log(err))
-  })
-  .patch(jsonParser, (req, res, next) => {
-    const { user_id,
-      board_title,
-      times_played,
-      date_created,
-      date_updated } = req.body
-    const boardsToUpdate = {
-      user_id,
-      board_title,
-      times_played,
-      date_created,
-      date_updated
-    }
-
-    const numberOfValues = Object.values(boardsToUpdate).filter(Boolean).length
-    if (numberOfValues === 0)
-      return res.status(400).json({
-        error: {
-          message: `Request body must content either 'title' or 'completed'`
-        }
-      })
-    console.log({
-      user_id,
-      board_title,
-      times_played,
-      date_created,
-      date_updated
-    })
-    BoardsService.updateBoards(
-      req.app.get('db'),
-      req.params.boards_id,
-      boardsToUpdate
-    )
-      .then(updatedboards => {
-        res.status(200).json(serializeBoards(updatedboards[0]))
-      })
-      .catch(err => console.log(err))
-  })
-
-boardsRouter
-  .route('/')
   .all((req, res, next) => {
     if (isNaN(parseInt(req.params.boards_id))) {
       return res.status(404).json({
@@ -134,51 +48,158 @@ boardsRouter
         next()
       })
       .catch(next)
-  })
-  .get((req, res, next) => {
-    res.json(serializeBoards(res.boards))
-  })
-  .delete((req, res, next) => {
-    BoardsService.deleteBoards(
-      req.app.get('db'),
-      req.params.id
-    )
-      .then(numRowsAffected => {
-        res.status(204).end()
+      .get((req, res, next) => {
+        res.json(serializeBoards(res.boards))
       })
-      .catch(next)
-  })
-  .patch(jsonParser, (req, res, next) => {
-    const { user_id,
-      board_title,
-      times_played,
-      date_created,
-      date_updated } = req.body
-    const boardsToUpdate = {
-      user_id,
-      board_title,
-      times_played,
-      date_created,
-      date_updated
-    }
-
-    const numberOfValues = Object.values(boardsToUpdate).filter(Boolean).length
-    if (numberOfValues === 0)
-      return res.status(400).json({
-        error: {
-          message: `Request body must content either 'title' or 'completed'`
+      .post(jsonParser, (req, res, next) => {
+        const { user_id,
+          board_title,
+          times_played
+        } = req.body
+        console.log(req.body)
+        const newBoards = {
+          user_id,
+          board_title,
+          times_played
         }
+
+        for (const [key, value] of Object.entries(newBoards))
+          if (value == null)
+            return res.status(400).json({
+              error: { message: `Missing '${key}' in request body` }
+            })
+
+
+        BoardsService.insertBoards(
+          req.app.get('db'),
+          newBoards
+        )
+          .then(boards => {
+            res
+              .status(201)
+              .location(path.posix.join(req.originalUrl, `/${boards.id}`))
+              .json(serializeBoards(boards))
+          })
+          .catch(next)
+      })
+      .delete((req, res, next) => {
+        console.log(req.params)
+        BoardsService.deleteBoards(
+          req.app.get('db'),
+          req.params.board_id
+        )
+          .then(numRowsAffected => {
+            console.log(numRowsAffected)
+            res.status(204).end()
+          })
+          .catch(err => console.log(err))
+      })
+      .patch(jsonParser, (req, res, next) => {
+        const { user_id,
+          board_title,
+          times_played,
+          date_created,
+          date_updated } = req.body
+        const boardsToUpdate = {
+          user_id,
+          board_title,
+          times_played,
+          date_created,
+          date_updated
+        }
+
+        const numberOfValues = Object.values(boardsToUpdate).filter(Boolean).length
+        if (numberOfValues === 0)
+          return res.status(400).json({
+            error: {
+              message: `Request body must content either 'title' or 'completed'`
+            }
+          })
+        console.log({
+          user_id,
+          board_title,
+          times_played,
+          date_created,
+          date_updated
+        })
+        BoardsService.updateBoards(
+          req.app.get('db'),
+          req.params.boards_id,
+          boardsToUpdate
+        )
+          .then(updatedboards => {
+            res.status(200).json(serializeBoards(updatedboards[0]))
+          })
+          .catch(err => console.log(err))
       })
 
-    BoardsService.updateboards(
-      req.app.get('db'),
-      req.params.boards_id,
-      boardsToUpdate
-    )
-      .then(updatedboards => {
-        res.status(200).json(serializeBoards(updatedboards[0]))
+    boardsRouter
+      .route('/')
+      .all((req, res, next) => {
+        if (isNaN(parseInt(req.params.boards_id))) {
+          return res.status(404).json({
+            error: { message: `Invalid id` }
+          })
+        }
+        BoardsService.getBoardsById(
+          req.app.get('db'),
+          req.params.boards_id
+        )
+          .then(boards => {
+            if (!boards) {
+              return res.status(404).json({
+                error: { message: `boards doesn't exist` }
+              })
+            }
+            res.boards = boards
+            next()
+          })
+          .catch(next)
       })
-      .catch(next)
-  })
+      .get((req, res, next) => {
+        res.json(serializeBoards(res.boards))
+      })
+      .delete((req, res, next) => {
+        BoardsService.deleteBoards(
+          req.app.get('db'),
+          req.params.id
+        )
+          .then(numRowsAffected => {
+            res.status(204).end()
+          })
+          .catch(next)
+      })
+      .patch(jsonParser, (req, res, next) => {
+        const { user_id,
+          board_title,
+          times_played,
+          date_created,
+          date_updated } = req.body
+        const boardsToUpdate = {
+          user_id,
+          board_title,
+          times_played,
+          date_created,
+          date_updated
+        }
 
-module.exports = boardsRouter
+        const numberOfValues = Object.values(boardsToUpdate).filter(Boolean).length
+        if (numberOfValues === 0)
+          return res.status(400).json({
+            error: {
+              message: `Request body must content either 'title' or 'completed'`
+            }
+          })
+
+        BoardsService.updateboards(
+          req.app.get('db'),
+          req.params.boards_id,
+          boardsToUpdate
+        )
+          .then(updatedboards => {
+            res.status(200).json(serializeBoards(updatedboards[0]))
+          })
+          .catch(next)
+      })
+
+    module.exports = boardsRouter
